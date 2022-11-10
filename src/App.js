5d6bc6cd8c8eac5f23f14d7028c116489c807f71
Gustavo Souza <gsouza.dev@gmail.com>
2022-11-10 05:33:08 -0300

    Adicionando os modos de jogo

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@
 import './App.css';
 
 // React
-import { useCallback } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 // Datas
 import { wordsList } from './data/words';
 
 // Components
 import StartApp from './components/StartApp';
+import Game from './components/Game';
+
+const modes = [
+  {id: 1, status: 'start'},
+  {id: 2, status: 'game'},
+  {id: 3, status: 'gameOver'}
+]
 
 function App() {
+  const [gameModes, setGameModes] = useState(modes[0].status);
+  const [words] = useState(wordsList);
+
+  const startGame = () => {
+    setGameModes(modes[1].status);
+  }
+
   return (
     <div className="App">
-      <StartApp />
+      {gameModes === 'start' && <StartApp startGame={startGame}  />}
+      {gameModes === 'game' && <Game />}
+      {gameModes === 'gameOver' && <StartApp />}
     </div>
   );
 }
--- src/components/StartApp.js
+++ src/components/StartApp.js
@@ -1,12 +1,12 @@
 // Styles
 import './StartApp.css';
 
-const StartApp = () => {
+const StartApp = ({startGame}) => {
   return (
     <div className="Start">
         <h1>Roda Rola Jekiti</h1>
         <p>Sua diversão está prestes a começar, aperte os cintos e clique no botão abaixo para começar!! :D</p>
-        <button>Começar o jogo</button>
+        <button onClick={startGame}>Começar o jogo</button>
     </div>
   )
 }
