d2dbf45c03a0dc14bc55b2ea2b80b74535603bf7
Gustavo Souza <gsouza.dev@gmail.com>
2022-11-17 05:38:27 -0300

    Alternando os Modos de Jogo

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ import { wordsList } from './data/words';
 // Components
 import StartApp from './components/StartApp';
 import Game from './components/Game';
+import GameOver from './components/GameOver';
 
+// Modos de Jogo
 const modes = [
   {id: 1, status: 'start'},
   {id: 2, status: 'game'},
@@ -21,15 +23,41 @@ function App() {
   const [gameModes, setGameModes] = useState(modes[0].status);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickedWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    console.log(category);
+
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+    
+    console.log(word);
+
+    return {category, word};    
+  }
+
   const startGame = () => {
+    pickedWordAndCategory();
     setGameModes(modes[1].status);
+  }
+
+  const verifyLetter = () => {
+    setGameModes(modes[2].status);
+  }
+
+  const retry = () => {
+    setGameModes(modes[0].status);
   }
 
   return (
     <div className="App">
       {gameModes === 'start' && <StartApp startGame={startGame}  />}
-      {gameModes === 'game' && <Game />}
-      {gameModes === 'gameOver' && <StartApp />}
+      {gameModes === 'game' && <Game verifyLetter={verifyLetter} />}
+      {gameModes === 'gameOver' && <GameOver retry={retry} />}
     </div>
   );
 }
